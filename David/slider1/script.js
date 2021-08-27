da1af866a1bb56deb59e0d7e0c7663553f5d24a5
Unuad <evtishinby@gmail.com>
2021-08-28 01:22:27 +0300

    Butt
    делал кнопки

--- David/slider1/script.js
+++ David/slider1/script.js
@@ -2,7 +2,15 @@ $(document).ready(function(){
 	var i = 0;
 	const ti = 5000
 	const ts = 1000
-	slide(i)
+	var isSlide = true
+	if (isSlide) slide(i)
+
+
+	$(".img").click(function() {
+		isSlide = false
+		if( i == 5 ) i = 0
+		$('.next').eq(i++).fadeIn(ts).delay(ti - ts).fadeOut(ts)
+	})
 
 	function slide(i) {
 		setTimeout(function run() {
@@ -11,5 +19,5 @@ $(document).ready(function(){
 		  if( i == 5 ) i = 0
 		  setTimeout(run, ti);
 		}, 1);
-	} 
+	}
 })
