fa2618509f30664952870522ba0742cbb990d6f7
Unuad <evtishinby@gmail.com>
2021-08-28 18:09:18 +0300

    Update script.js
    Fail

--- David/slider1/script.js
+++ David/slider1/script.js
@@ -4,20 +4,39 @@ $(document).ready(function(){
 	const ts = 1000
 	var isSlide = true
 
+	slider()
+	
 
-
-	$(".next").click(function() {
-		isSlide = false
-		if( i == 5 ) i = 0
-		$('.img').eq(i++).fadeIn(ts).delay(ti - ts).fadeOut(ts)
-	})
-
-	if (isSlide){
+	function slider(){
 		setTimeout(function run() {
-		  $('.img').eq(i).fadeIn(ts).delay(ti - ts).fadeOut(ts)
+			
+		  $('.img').eq(i).fadeIn(ts).delay(ti - ts)
+
+		  
+		  if (isSlide){
 		  i++
-		  if( i == 5 ) i = 0
+		  if( i === 5 ) i = 0
 		  setTimeout(run, ti);
+		   $('.img').fadeOut(ts)
+		}
 		}, 1);
-	 }
+
+		}
+
+		$(".next").click(function() {
+		isSlide = false
+		if( i === 5 ) i = 0
+		$('.img').eq(i++).fadeIn(ts).delay(ti - ts).fadeOut(ts)
+		setTimeout(isSlide == true, ti)
+
+
+		})
+		$(".prev").click(function() {
+		isSlide = false
+		if( i === 0 ) i = 5
+		$('.img').eq(i--).fadeIn(ts).delay(ti - ts).fadeOut(ts)
+		setTimeout(isSlide == true, ti)
+		})
+
+	 
 })
