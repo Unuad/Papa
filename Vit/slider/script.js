e6e6de884e7a0a248b920813f48888740a060924
mrvit <viteft@gmail.com>
2021-09-01 19:16:12 +0300

    закоментировал
    script.js

--- Vit/slider/script.js
+++ Vit/slider/script.js
@@ -1,47 +1,46 @@
 $(document).ready(function(){
-	let i = 0
-	const ti = 5000
-	const ts = 500
-	var timeInt  = 0
-	var b = 0
-	var c = 0
-	var d = $('.img').length - 1
+	let i = 0                               // счетчик слайдов передаваемый в функцию run()
+	const ti = 5000                         // время показа слайда  
+	const ts = 500                          // время появления / затухания слайда
+	var timeInt  = 0                        // номер сессии setTimeout()
+	var b = 0                               // счетчик слайдов для кнопки "вперед"
+	var c = 0                               // счетчик слайдов для кнопки "назад"
+	var d = $('.img').length - 1            // количество слайдов
 	run(i)
 
-		$('#next').click(function() {
+		$('#next').click(function() {       // обработчик кнопки "вперед"
 			clearAll()
 			run(b)
 		})
-		$('#prev').click(function() {
+		$('#prev').click(function() {       // обработчик кнопки "назад"
 			clearAll()
 	  		c == 0 ? c = d : c--
 			run(c)
 		})
-		$('.kropka').click(function() {
+		$('.kropka').click(function() {       // обработчик кнопок слайдов
 			clearAll()
 			let wq = $(this).index()
 			run(wq)
 		})
 
-	function run(i) {
-			  c = i
-	  $('.img').eq(i)
-	  .fadeIn(ts, function() {
-	  	$('.kropka').eq(c).html('◉')
+	function run(i) {                         // запуск слайдера
+	  c = i
+	  $('.img').eq(i)                         // получаем номер слайда
+	  .fadeIn(ts, function() {                // появление слайда
+	  	$('.kropka').eq(c).html('◉')         // подсвечивание кнопки слайда
 	  })
-	  .delay(ti - ts)
-	  .fadeOut(ts)
+	  .delay(ti - ts)                         // показ слайда
+	  .fadeOut(ts)                            // затухание слайда
 	 
-	  i == d ? i = 0 : i++
-	  b = i
-	  timeInt = setTimeout(run, ti, i);
-	  $('.kropka').html('◎')
+	  i == d ? i = 0 : i++                    // счетчик слайдов
+	  b = i                                   // счетчик для кнопки "назад"
+	  timeInt = setTimeout(run, ti, i);       // рекурсивно запускаем run() после показа слайда
+	  $('.kropka').html('◎')                 // сбрасываем подсвечивание кнопок
 	}
 
-	function clearAll(){
-		$('.img').dequeue()
-  		clearTimeout(timeInt)
+	function clearAll(){                      // выключаем слайдер
+		$('.img').dequeue()                   // выключаем delay()
+  		clearTimeout(timeInt)                 // выключаем setTimeout()
 	}
-	//◎◉❂
-	//$('#test').html(wq)
+											//◎◉❂
 })
